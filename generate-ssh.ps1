1b41b18e9e40658bdccc23e09091bd7cc6d26d44
Jamie Bowman <mrjamiebowman@gmail.com>
2021-02-11 13:51:51 -0500

    updated ps1 script

--- generate-ssh.ps1
+++ generate-ssh.ps1
@@ -1,4 +1,4 @@
 # if you like you can generate your own ssh key and use that to authenticate.
-# make sure you update config/authorized_keys
+# make sure you update config/authorized_keys and rebuild the docker images
 
 ssh-keygen -t rsa -b 4096 -f key
