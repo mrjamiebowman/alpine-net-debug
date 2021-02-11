9b4412e207adb396d66e1f7ac589e1b11fc6f0eb
Jamie Bowman <mrjamiebowman@gmail.com>
2021-02-11 13:46:45 -0500

    moved to alpine folder

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Alpine: .NET Debug Base Images (vsdbg)
-.NET Alpine debug base image
+.NET Alpine debug base image used for debugging containers running in minkube/kubernetes cluster.
+
+## How to Use
+
+### SSH into Container Running in Kubernetes
--- build.ps1
+++ build.ps1
@@ -1,4 +1,10 @@
-docker build --no-cache -f net-3.1\Dockerfile -t mrjamiebowman/alpine-net-debug:net3.1 .
-#docker build --no-cache -f net5\Dockerfile -t mrjamiebowman/alpine-net-debug:net5 .
+
+# alpine
+docker build --no-cache -f alpine\net-3.1\Dockerfile -t mrjamiebowman/alpine-net-debug:net3.1 .
+docker build --no-cache -f alpine\net-5\Dockerfile -t mrjamiebowman/alpine-net-debug:net5 .
+
+# debian
+docker build --no-cache -f debian\net-3.1\Dockerfile -t mrjamiebowman/alpine-net-debug:net3.1 .
+docker build --no-cache -f debian\net-5\Dockerfile -t mrjamiebowman/alpine-net-debug:net5 .
 
 docker images | findstr alpine-net-debug
